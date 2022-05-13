582953bdbe412c8b8c6d8b4925813d7560c99fd8
Eko1101 <84501519+Eko1101@users.noreply.github.com>
2022-05-13 14:07:49 +0200

    Update MasterpasswortChange.py

--- MasterpasswortChange.py
+++ MasterpasswortChange.py
@@ -5,7 +5,7 @@ import sqldb
 
 
 class FrameChangempw(wx.Frame):
-
+"""Creates the GUI for Masterpassword Change. Ekrem"""
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"Vertragsverwaltung", pos=wx.DefaultPosition,
                           size=wx.Size(600, 311), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
